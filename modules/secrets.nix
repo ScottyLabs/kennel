b2059026a0dbfaa9f801d423a925d8c96caa710a
Anish Pallati <i@anish.land>
2026-06-09 11:38:06 -0700

    fix: drop dead secretspec provider/profile config

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -50,29 +50,16 @@ let
 in
 {
   options.scottylabs.secrets = {
-    enable = lib.mkEnableOption "secretspec integration for local secret resolution";
-
-    host = lib.mkOption {
-      type = lib.types.str;
-      default = "secrets2.scottylabs.org";
-      description = "OpenBao server hostname";
-    };
-
-    profile = lib.mkOption {
-      type = lib.types.str;
-      default = "dev";
-      description = "secretspec profile for local development";
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Install the openbao and secretspec CLIs and set BAO_ADDR for OpenBao";
     };
   };
 
   config = lib.mkIf (config.scottylabs.enable && cfg.enable) {
     packages = [ pkgs.openbao secretspec ];
 
-    env.BAO_ADDR = "https://${cfg.host}";
-
-    secretspec = {
-      provider = "vault://${cfg.host}/secret";
-      profile = cfg.profile;
-    };
+    env.BAO_ADDR = "https://secrets2.scottylabs.org";
   };
 }
