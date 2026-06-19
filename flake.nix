3b9034f21011e7a7ea4a1a6c0889ed379e54efef
Anish Pallati <i@anish.land>
2026-06-19 04:33:03 -0400

    feat: add openbao login bootstrap app
    Signed-off-by: Anish Pallati <i@anish.land>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,31 @@
 {
   description = "ScottyLabs shared devenv configuration";
 
-  outputs = { self }: {
-    devenvModules.default = import ./modules;
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devenvModules.default = import ./modules;
+
+      # Authenticate to OpenBao before any project shell can build
+      apps = forAllSystems (pkgs: {
+        login = {
+          type = "app";
+          program = "${pkgs.writeShellScript "bao-login" ''
+            export BAO_ADDR=https://secrets2.scottylabs.org
+            exec ${pkgs.openbao}/bin/bao login -method=oidc
+          ''}";
+        };
+      });
+    };
 }
