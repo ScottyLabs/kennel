2fd257d6d3905ba04be48a0e9b817d01bbf03be7
Anish Pallati <i@anish.land>
2026-06-20 07:26:03 -0400

    feat: set dev APP_URL from ricochet.appUrl
    Signed-off-by: Anish Pallati <i@anish.land>

--- modules/ricochet.nix
+++ modules/ricochet.nix
@@ -13,10 +13,16 @@ let
 in
 {
   options.scottylabs.ricochet = {
-    enable = lib.mkEnableOption "local OAuth relay for development";
+    enable = lib.mkEnableOption "Local OAuth relay for development";
+
+    appUrl = lib.mkOption {
+      type = lib.types.str;
+      description = "Public URL of this service in development, exported as APP_URL";
+    };
   };
 
   config = lib.mkIf (config.scottylabs.enable && cfg.enable) {
     processes.ricochet.exec = "RICOCHET_DEV=1 RICOCHET_BIND=127.0.0.1:${toString port} ${ricochet}/bin/ricochet";
+    env.APP_URL = cfg.appUrl;
   };
 }
