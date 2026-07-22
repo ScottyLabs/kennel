ae4cf56c2375c51169c481dc8cfac078e45ff494
Anish Pallati <i@anish.land>
2026-07-22 03:23:02 -0400

    refactor: remove terranix base config from devenv
    Signed-off-by: Anish Pallati <i@anish.land>

--- flake.nix
+++ flake.nix
@@ -76,25 +76,10 @@
             mdbook build ${src} --dest-dir "$out"
           '';
         buildOptionsDoc = import ./lib/options-doc.nix { inherit pkgs; };
-        mkAtlantisYaml = import ./lib/mk-atlantis-yaml.nix { inherit pkgs; };
-      };
-
-      # Terranix modules shared across ScottyLabs infrastructure/governance repos
-      terranixModules.s3Backend = {
-        terraform.backend.s3 = {
-          bucket = "tofu-state";
-          endpoints.s3 = "http://127.0.0.1:3900";
-          region = "us-east-1";
-          use_path_style = true;
-          skip_credentials_validation = true;
-          skip_requesting_account_id = true;
-          skip_metadata_api_check = true;
-          skip_region_validation = true;
-        };
       };
     in
     {
-      inherit devenvModules mkLib terranixModules;
+      inherit devenvModules mkLib;
 
       packages = forAllSystems (pkgs: {
         options-doc = (mkLib pkgs).buildOptionsDoc {
